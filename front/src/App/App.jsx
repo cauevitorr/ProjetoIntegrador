2238796514ab4a3baae9dff82b0946d6f8457e22
Cauê Vitor <138812443+cauevitorr@users.noreply.github.com>
2024-11-25 18:15:09 -0300

    Add files via upload

---
+++ front/src/App/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import axios from "axios"
+import Footer from "../components/Footer.jsx"
+
+function App() {
+  return (
+    <>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ front/src/styles/style.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const FooterMain = styled.footer``
+
+export const Div01 = styled.div`
+    width: 100%;
+    padding: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #D9D9D9;
+    color: #0A1626;
+    box-sizing: border-box;
+    font-size: 14px;
+`
+
+export const Div02 = styled.div`
+    width: 100%;
+    padding: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #0A1626;
+    box-sizing: border-box;
+`
